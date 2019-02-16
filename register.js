5afeaacb85a0003e295a565cf32c53f7afdda245
liuswz <2639147439@qq.com>
2019-02-16 22:32:14 +0800

    王磊

--- register.js
+++ register.js
@@ -10,46 +10,110 @@ new Vue({
             shopEmail: null,
             shopPhone: null,
             mchId: null,
-            apiKey: null
+            apiKey: null,
+            notice:null,
+            photoUrl:null
         }
 
     },
     methods: {
+        look_photo: function (event) {
+            var file = $('input[type="file"]')[0].files[0];
+            console.log(file);
+            var reader = new FileReader();
+            //使用该对象读取file文件
+            reader.readAsDataURL(file);
+            //读取文件成功后执行的方法函数
+            reader.onload = function (e) {
+                //读取成功后返回的一个参数e，整个的一个进度事件
+
+                $('#imgshow').get(0).src = e.target.result;
+            }
+        },
+        upload_photo: function (event) {
+
+            var file = $('input[type="file"]')[0].files[0];
+            var fileExt = file.name.substring(file.name.lastIndexOf("."))
+                .toLowerCase();
+
+            if (!this.checkFileExt(fileExt)) {
+                alert("您上传的文件不是图片,请重新上传！");
+                //img.value = "";
+                return;
+            } else {
+
+                var photo_name = new Date().getTime() + "" + parseInt(Math.random() * 10000 + 1);
+                client.multipartUpload("/shop/" + photo_name, file).then(function (result) {
+
+                    //判断系统
+                    var agent = navigator.userAgent.toLowerCase();
+                    var isMac = /macintosh|mac os x/i.test(navigator.userAgent);
+                    var requestUrl = result.res.requestUrls[0];
+                    if (isMac) {
+                    
+                        console.log(requestUrl);
+                        if (requestUrl) {
+                            var imgUrl = requestUrl.substring(0,requestUrl.indexOf("?"));
+                            this.photoUrl = imgUrl;
+                            console.log(imgUrl);
+
+
+                            alert("上传成功");
+                        }
+                    }else{
+                        if (requestUrl) {
+                            this.photoUrl = requestUrl;
+
+
+                            alert("上传成功");
+                        }
+                    }
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            }
+
+        },
+        checkFileExt(ext) {
+            if (!ext.match(/.jpg|.gif|.png|.bmp/i)) {
+                return false;
+            }
+            return true;
+        },
         register: function (e) {
-            this.errors = [];
+        
             if (!this.registDto.username) {
-                this.errors.push('用户名不能为空');
+                alert('用户名不能为空');
             } else if (!this.validUsername(this.registDto.username)) {
-                this.errors.push('用户名只能输入5-20个 以字母开头、可带数字');
+                alert('用户名只能输入5-20个 以字母开头、可带数字');
             }
             else if (!this.registDto.password) {
-                this.errors.push('密码不能为空');
+                alert('密码不能为空');
             } else if (!this.validPassword(this.registDto.password)) {
-                this.errors.push('密码只能输入6-20个字母、数字、下划线 ');
+                alert('密码只能输入6-20个字母、数字、下划线 ');
             }
             else if (!this.registDto.shopName) {
-                this.errors.push('店名不能为空');
+                alert('店名不能为空');
             }
             else if (!this.registDto.shopAddress) {
-                this.errors.push('地址不能为空');
+               alert('地址不能为空');
             }
 
             else if (!this.registDto.shopEmail) {
-                this.errors.push('邮件不能为空');
+                alert('邮件不能为空');
             } else if (!this.validEmail(this.registDto.shopEmail)) {
-                this.errors.push('邮件格式不正确 ');
+                alert('邮件格式不正确 ');
             }
             else if (!this.registDto.shopPhone) {
-                this.errors.push('电话号码不能为空');
+                alert('电话号码不能为空');
             } else if (!this.validPhone(this.registDto.shopPhone)) {
-                this.errors.push('电话号码格式不正确');
+                alert('电话号码格式不正确');
+            }else if(!this.registDto.photoUrl){
+                alert('图片不能为空');
             }
 
 
-            if (!this.errors.length) {
-                return false;
-            }
-            e.preventDefault();
+            
             this.$http.post('http://localhost:9001/consumer/shopdetail/register',  this.registDto, { emulateJSON: true }).then(function (response) {
                 console.log(response);
             }, function (res) {
